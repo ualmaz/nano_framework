be952b77070b095fc2aff57670f8e4f3c7d2c2b2
kazak <kazak@ujoshua@protonmail.com>
2018-02-22 21:37:36 +0600

    Make renderer show replace placeholders

--- renderer.py
+++ renderer.py
@@ -1,6 +1,16 @@
-def render(file_name):
+def render(file_name, context=None):
     with open(file_name, 'r') as html_file:
         html = html_file.read()
+        if context:
+            html = _replacer(context, html)
         return html
 
-
+def _replacer(context, html):
+    """
+    html = 'dvdfv{text}sdc{second}sdc'
+    context = {'text': '!avt!', 'second': '!aaa!'}
+    """
+    for k in context.keys():
+        ready_key = '{' + k + '}'
+        html = html.replace(ready_key, context[k])
+    return html
